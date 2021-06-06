c0214eb81cf36ce0ab0eed562c9717e29f6a3e88
Shilpi Karmakar <56488690+shilpikarmakar@users.noreply.github.com>
2021-06-06 23:57:42 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>About</h1>
+      <p>
+        Hello Everyone, This is Shilpi Karmakar currently persuing B.Tech. This
+        Bootcamp has been beneficial to me in the sense that it taught me the
+        basics of Javascript and React js which will now help in extendind my
+        knowledge in the course further.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ index.js
@@ -0,0 +1,9 @@
+import App from "./components/App";
+import React from "react";
+var ReactDom = require("react-dom");
+ReactDom.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
